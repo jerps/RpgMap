8c35067734bdde0126ca2c5fd6c90594e76a2aa0
jerps <john.erps@gmail.com>
2017-12-05 03:12:03 +0100

    1.1.0

--- Docs/content/notice.js
+++ Docs/content/notice.js
@@ -3,11 +3,11 @@ document.write('\
 <h1 id="notice">Notice</h1>\
 <br>\
 <p>\
-<b>Last version is V2017.3 (2017/09/06)</b>&nbsp;&gt;&gt;&gt;&nbsp;See <a href="https://github.com/jerps/RpgMap/blob/master/README.md">changes</a> at GitHub.\
+<b>Last version is 1.1.0 (2017/12/05)</b>&nbsp;&gt;&gt;&gt;&nbsp;See <a href="https://github.com/jerps/RpgMap/blob/master/README.md">changes</a> at GitHub.\
 </p>\
 <br>\
 <p>\
-Send mail concerning RpgMap to rpgmap0@gmail.com.\
+Send mail concerning RpgMap to john@erps.me.\
 </p>\
 \
 ');
