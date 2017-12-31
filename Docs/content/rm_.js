66cfe96d9a750f65554ea7b7f4875b0c799316ae
jerps <john.erps@gmail.com>
2017-12-31 09:23:37 +0100

    Misc. changes

--- Docs/content/rm_.js
+++ Docs/content/rm_.js
@@ -29,31 +29,31 @@ value with user data <code>100</code>. When creating maps there is no <code>opt<
 so <a href="#rm_"><code>rm_</code></a> must always be used instead.<br>\
 <span class="bullet1">&#8226;</span>&nbsp;Specify <code>90</code> or <code>91</code> for <code>opt</code> to clear (remove) special properties or user \
 data.<br>\
-<span class="bullet1">&#8226;</span>&nbsp;<b>&gt; Reversed Order</b><br>\
-If a map or value has the "reversed order" property, then when it\'s \
+<b>&gt; Reversed Order</b><br>\
+<span class="bullet1">&#8226;</span>&nbsp;If a map or value has the "reversed order" property, then when it\'s \
 compared to other maps or values the sort order is reversed. Value \
 <code>*hival</code> is then the lowest in order, and vice versa. When comparing \
 two objects, reversed order is in effect when at least one has the \
 reversed order property set, otherwise normal order is in effect.<br>\
-<span class="bullet1">&#8226;</span>&nbsp;<b>&gt; Case Insensitive Order</b><br>\
-If <code>obj</code> is character data or a string, with normal sort sequence, \
+<b>&gt; Case Insensitive Order</b><br>\
+<span class="bullet1">&#8226;</span>&nbsp;If <code>obj</code> is character data or a string, with normal sort sequence, \
 then the "case insensitive order" can be set. If set, then when <code>obj</code> \
 is compared with other character data or strings this is done without \
 difference between uppercase and lowercase characters. Like reversed \
 order, case insensitive order is in effect when two objects are compared \
 and at least one of them has the case insensitive order property.<br>\
-Some extra processing is required when a case insensitive comparison is \
+<span class="bullet1">&#8226;</span>&nbsp;Some extra processing is required when a case insensitive comparison is \
 done. Especially when setting this property for strings, as they need to \
 be converted to the job CCSID first, before being compared case insensitive.<br>\
-<span class="bullet1">&#8226;</span>&nbsp;<b>&gt; User Data</b><br>\
-A map or value can be associated with user data, which is a positive integer \
+<b>&gt; User Data</b><br>\
+<span class="bullet1">&#8226;</span>&nbsp;A map or value can be associated with user data, which is a positive integer \
 value between <code>1</code> and <code>65.000</code> that is application specific. Like attributes \
 (<a href="#rm_sav"><code>rm_sav</code></a>), user data is used to store meta data.<br>\
-Associating user data does not increase memory usage.<br>\
-Procedure <a href="#rm_usrd"><code>rm_usrd</code></a> returns the user data for a map or value.<br>\
-The user data is specified as a negative integer; specifying -1 means that \
-<a href="#rm_usrd"><code>rm_usrd</code></a> returns 1.<br>\
-If <code>opt</code> is <code>91</code> then the user data is cleared, i.e. <a href="#rm_usrd"><code>rm_usrd</code></a> returns <code>0</code>.\
+<span class="bullet1">&#8226;</span>&nbsp;Associating user data does not increase memory usage.<br>\
+<span class="bullet1">&#8226;</span>&nbsp;Procedure <a href="#rm_usrd"><code>rm_usrd</code></a> returns the user data for a map or value.<br>\
+<span class="bullet1">&#8226;</span>&nbsp;The user data is specified as a negative integer; specifying <code>-1</code> means that \
+<a href="#rm_usrd"><code>rm_usrd</code></a> returns <code>1</code>.<br>\
+<span class="bullet1">&#8226;</span>&nbsp;If <code>opt</code> is <code>91</code> then the user data is cleared, i.e. <a href="#rm_usrd"><code>rm_usrd</code></a> returns <code>0</code>.\
 </p>\
 \
 ');
