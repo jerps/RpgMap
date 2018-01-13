35a0f4529b42dc746b0d871992f0b2aa76609674
jerps <john@erps.me>
2018-01-13 11:47:52 +0100

    Misc. changes

--- Docs/content/threads.js
+++ Docs/content/threads.js
@@ -4,12 +4,12 @@ document.write('\
 <br>\
 <p>\
 RpgMap is not threadsafe. That means that when multiple threads are active in a job that uses RpgMap, care should \
-be taken that no two threads execute an RpgMap procedure at the same time. This can be done using synchronization \
-constructs such as locks, sempahores or mutexes. One could also simply specify control-options keyword <code>thread(*serialize)</code> \
-for an RPG module, and make sure that all access to RpgMap is done through that module. This ensures that only one \
-thread accesses RpgMap at any moment in time. This solution has a coarse granularity. It hinders performance \
-when many threads are waiting and there is a greater risk for deadlocks. However, for most practical situations \
-using <code>thread(*serialize)</code> would be good enough.\
+be taken that no two threads execute any RpgMap procedure within the same activation group at the same time. \
+This can be done using synchronization constructs such as locks, sempahores or mutexes. One could also simply specify \
+control-options keyword <code>thread(*serialize)</code> for an RPG module, and make sure that all access to RpgMap is \
+done through that module. This ensures that only one thread accesses RpgMap at any moment in time. This solution has \
+a coarse granularity. It hinders performance when many threads are waiting and there is a greater risk for deadlocks. \
+However, for most practical purposes using <code>thread(*serialize)</code> would be good enough.\
 </p>\
 \
 ');
