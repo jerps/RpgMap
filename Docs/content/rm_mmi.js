f399d88b8bb3f4f051f6ed32ee877f12a0da5c74
jerps <john.erps@gmail.com>
2018-01-16 21:01:51 +0100

    Added 'opt' to rm_mmi

--- Docs/content/rm_mmi.js
+++ Docs/content/rm_mmi.js
@@ -4,7 +4,8 @@ document.write('\
 <pre class="proclongnm">rm_make_map_immutable</pre>\
 <pre class="procsig">\
 &lt;&lt;pointer<br>\
-map pointer value<br>\
+map pointer  value<br>\
+opt char(10) value options(*nopass)<br>\
 </pre>\
 <p>\
 <span class="bullet1">&sstarf;</span>&nbsp;Make <code>map</code> immutable, and return <code>map</code>.<br>\
@@ -13,6 +14,8 @@ into or removed from the map. If a key/item pair is inserted into or removed \
 from an immutable map then escape message RM00005 is sent.<br>\
 <span class="bullet1">&sstarf;</span>&nbsp;All contained <strong>automatically</strong> disposed maps (thus including vectors) are also \
 made immutable, recursively.<br>\
+<span class="bullet1">&sstarf;</span>&nbsp;If <code>opt</code> is passed and is <code>\'*mm\'</code>"*mm" then all contained <strong>manually<strong> disposed maps are \
+also made immutable, recursively, like contained automatically disposed maps.<br>\
 <span class="bullet1">&sstarf;</span>&nbsp;If <code>map</code> is already immutable then this operation has no effect.<br>\
 <span class="bullet1">&sstarf;</span>&nbsp;This operation can not be undone; an immutable map can not be made mutable again.<br>\
 <span class="bullet1">&sstarf;</span>&nbsp;When a map is copied (<a href="#rm_cpy"><code>rm_cpy</code></a>) then the new copy is mutable.\
