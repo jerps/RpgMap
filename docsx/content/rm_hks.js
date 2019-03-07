e788c6f9b84d5e5705dc8a427c05543a2736f9f0
john erps <rpgmap@johnerps.com>
2019-03-07 04:03:02 +0100

    v2.00.0

--- docsx/content/rm_hks.js
+++ docsx/content/rm_hks.js
@@ -9,9 +9,9 @@ key pointer  value<br>\
 opt char(10) value options(*nopass)<br>\
 </pre>\
 <p>\
-<span class="bullet1">&#9679;</span>&nbsp;If <code>opt</code> is <code>\'*ge\'</code> or not passed, then return <code>*on</code> if <code>map</code> has keys that \
-are equal to or greater than <code>key</code>, else return <code>*off</code>. If <code>opt</code> is <code>\'*le\'</code> \
-then return <code>*on</code> if <code>map</code> has keys that are equal to or less than <code>key</code>.<br>\
+<span class="bullet1">&#9679;</span>&nbsp;If <code>opt</code> is <code>\'*ge\'</code> or not passed, then return <code>*on</code> if \
+<code>map</code> has 1 or more keys that are equal to or greater than <code>key</code>, else return <code>*off</code>. If <code>opt</code> \
+is <code>\'*le\'</code> \then return <code>*on</code> if <code>map</code> has 1 or more keys that are equal to or less than <code>key</code>.<br>\
 <span class="bullet1">&#9679;</span>&nbsp;This procedure disposes <code>key</code> automatically, unless <code>key</code> is a manually \
 disposed map, or a value/map contained in a map.\
 </p>\
